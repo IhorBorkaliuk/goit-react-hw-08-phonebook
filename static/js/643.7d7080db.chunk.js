9fd54802d476e8927a2acfe4e4aff1214ff6bf54
IhorBorkaliuk <99047545+IhorBorkaliuk@users.noreply.github.com>
2022-11-28 21:43:31 +0000

    Deploying to gh-pages from @ IhorBorkaliuk/goit-react-hw-08-phonebook@8362205b91fa6c1c2493d8ea5c2038259690a0de 🚀

---
+++ static/js/643.7d7080db.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[643],{643:function(e,t,r){r.r(t),r.d(t,{Register:function(){return c}});var n=r(184),c=function(){return(0,n.jsx)("div",{children:(0,n.jsx)("h1",{children:"Registration Page"})})}}}]);
+//# sourceMappingURL=643.7d7080db.chunk.js.map
